8b844f260224e6c11124ee08937370a1b2d77920
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-27 18:33:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,19 @@
 class School
-	attr_accessor :roster, :name, :grade 
-	attr_reader :school 
-	
+	attr_accessor :roster, :name, :grade
+	attr_reader :school
+
 	def initialize(school)
     @school = school
   end
-  
+
   def roster
   	roster = {}
-  end 
-  
+  end
+
   def add_student(name,grade)
   	roster[grade] = []
   	roster[grade] << name
-  		
+
   end
 
 end
