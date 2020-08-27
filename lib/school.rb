6c05bf993184e1f0b3747708cd782a9a3c804e0a
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-27 18:28:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,12 +12,15 @@ class School
     roster[level] << student
   end
 
-  def grade(level)
-    roster.detect do | x, y|
-      if x == level
-        return y
-      end
-  end
+    def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+  
+end 
 
 
 end
